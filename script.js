7c3d6bda20e58d9f6b559392e9bef9d195d91647
Glitch (heavenly-cormorant) <none>
2018-05-21 21:29:59 +0000

    🎡🌴 Checkpoint ./script.js:293015/2984 ./index.html:293015/28

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-// Training data
-const training = {
-  x: [1, 2, 3, 4],
-  y: [5, 12, 8, 10]
+go();
+
+function plot(training, prediction) {
+  const trace1 = {
+    x: training.x,
+    y: training.y,
+    mode: 'lines+markers',
+    name: 'Training Data',
+    marker: { size: 12, color: '#29B6F6' }
+  };
+  
+   
+  // Plot it.
+  const trace2 = {
+    x: predictions.x,
+    y: predictions.y,
+    mode: 'markers',
+    name: 'Guess',
+    marker: { size: 12, color: '#F06292' }
+  };
+
+  Plotly.newPlot('graph', [trace1, trace2], layout);
+
 }
 
 const predictions = { x: [], y: []};
@@ -12,26 +31,43 @@ const trace = {
   y: training.y,
   mode: 'lines+markers',
   name: 'Training Data',
-  marker: { size: 12 }
+  marker: { size: 12, color: '#29B6F6' }
 };
 
-var layout = {
-};
+var layout = {};
+}
+  
+  
+function go() {
+  // Training data
+  const training = {
+    x: [1, 2, 3, 4],
+    y: [5, 12, 8, 10]  
+  }
+  plot(training);
+  
+  const model = learn(training.x, training.y);  
+  predict(model);
+}
 
-// Define a model for linear regression.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+function learn(xData, yData) {
+  // Define a model for linear regression.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-// Generate some synthetic data for training.
-const xs = tf.tensor2d(training.x, [4, 1]);
-const ys = tf.tensor2d(training.y, [4, 1]);
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
-model.fit(xs, ys).then(() => predict(5));
+  // Generate some synthetic data for training.
+  const xs = tf.tensor2d(xData, [4, 1]);
+  const ys = tf.tensor2d(yData, [4, 1]);
 
-function predict(what) {
+  model.fit(xs, ys).then(() => predict(5));
+  return model;
+}
+
+function predict(model, what) {
   if (!what) {
     what = document.getElementById('input').value;
   }
@@ -41,15 +77,5 @@ function predict(what) {
   
   predictions.x.push(what);
   predictions.y.push(prediction.get());
-  
-  // Plot it.
-  const trace2 = {
-    x: predictions.x,
-    y: predictions.y,
-    mode: 'markers',
-    name: 'Guess',
-    marker: { size: 12 }
-  };
-
-  Plotly.newPlot('graph', [trace, trace2], layout);
+ 
 }
