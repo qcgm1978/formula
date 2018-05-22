302b981216524ed1bef3a21f63f9a7f5a6ecdeee
Glitch (hello-tensorflow) <none>
2018-05-22 17:33:10 +0000

    🍶📚 Checkpoint ./script.js:293015/8

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function plot() {
     x: Data.prediction.x,
     y: Data.prediction.y,
     mode: 'lines+markers',
-    name: 'Prediction',
+    name: 'Initial Prediction',
     marker: { size: 12, color: '#F06292' }
   };
   
