99358e48e798be6cb68d1f52a320d9145634c3f1
Philipp <zhanghongliang@192.168.1.101>
2020-08-29 09:21:06 +0800

    adjust value

--- __test__/js.spec.js
+++ __test__/js.spec.js
@@ -1,25 +1,23 @@
 const tf = require('@tensorflow/tfjs-node')
 // tf = require('tfjs');
-it(`tidy`, () => {
+it(`tf`, () => {
   expect(tf instanceof Object).toBeFalsy()
   expect(tf.tidy).toBeInstanceOf(Function)
   expect(tf.scalar).toBeInstanceOf(Function);
+})
+it(`The optimizer will then adjust the value of a to minimize this loss`, () => {
   const a = tf.variable(tf.scalar(Math.random()));
-  const data1 = a.dataSync()[0];
+  const data1 = a.dataSync();
   const optimizer = tf.train.sgd(.5);
+  // Executes f() and minimizes the scalar output of f() by computing gradients of y with respect to the list of trainable variables provided by varList. If no list is provided, it defaults to all trainable variables.
+  const returnCost = true
+  const varList = [a]
   optimizer.minimize(() => {
-    pred = tf.tidy(() => {
-      // The value returned inside the tidy function will return
-      // through the tidy, in this case to the variable pred.
-      const one = tf.scalar(1);
-      return a.add(one);
-    });
-    // Need to return the loss i.e how bad is our prediction from the 
-    // correct answer. The optimizer will then adjust the value of a
-    // to minimize this loss.
-    return pred.sub(1).square().mean();
-  });
-  const data2 = a.dataSync()[0];
+    // Need to return the loss i.e how bad is our prediction from the correct answer.
+    // return pred.sub(1).square().mean();
+    return a;
+  }, returnCost, varList);
+  const data2 = a.dataSync();
   expect(data2).not.toEqual(data1)
   console.log(data1, data2)
 });
